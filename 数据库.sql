e7a65f1d34d703bddb40ecd06df4b39abcecdb38
星 <i@star2000.work>
2019-05-19 19:10:03 +0800

    添加初始数据

--- 数据库.sql
+++ 数据库.sql
@@ -3,18 +3,21 @@ drop database if exists msg;
 create database msg;
 use msg;
 
-create table `留言` (
-    `编号` int unsigned not null primary key auto_increment,
-    `日期` date not null,
-    `标题` varchar(20) not null,
-    `内容` text not null,
-    `邮件` varchar(60) not null,
-    `ip` varchar(15) not null
+create table 留言 (
+  编号 int(10) unsigned not null auto_increment primary key,
+  时间 timestamp not null,
+  标题 varchar(20) not null,
+  内容 text not null,
+  邮件 varchar(60) not null,
+  ip varchar(15) not null
 );
 
-create table `管理` (
-    `编号` int unsigned not null primary key auto_increment,
-    `名字` varchar(20) not null,
-    `密码` varchar(32) not null,
-    `盐` char(4) not null
-)
+insert into 留言(标题, 内容, 邮件, ip) values ('测试','测试留言板','i@star2000.work','127.0.0.1');
+
+create table 管理 (
+  编号 int(10) unsigned not null auto_increment primary key,
+  名字 varchar(20) not null,
+  密码 varchar(40) not null
+);
+
+insert into 管理(名字, 密码) values ('李星',sha1(1753304));
