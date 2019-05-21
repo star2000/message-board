114c46bbe715b6aa312db00a8fe356ff6d569e14
星 <i@star2000.work>
2019-05-21 14:56:54 +0800

    使视图可控

--- 框架/视图.php
+++ 框架/视图.php
@@ -11,14 +11,16 @@ class 视图
      * @var string $模板
      * @var array $数据
      */
-    public function 渲染(array $数据 = [])
+    public function 渲染(array $数据 = [], string $应用 = '', string $视图 = '')
     {
+        $应用 = $应用 ?: $GLOBALS['应用'];
+        $视图 = $视图 ?: $GLOBALS['行为'];
         array_walk_recursive($数据, function (&$值) {
             if (is_string($值)) {
                 $值 = nl2br(htmlentities($值));
             }
         });
         extract($数据, EXTR_OVERWRITE);
-        require __DIR__ . '/../应用/' . $GLOBALS['应用'] . '/视图/' . $GLOBALS['行为'] . '.php';
+        require __DIR__ . "/../应用/$应用/视图/$视图.php";
     }
 }
