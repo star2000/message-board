3fa3c1bd970c69cbbf99dd9f76de0eb7dcbedaa2
星 <i@star2000.work>
2019-05-21 14:54:20 +0800

    管理改为管理员

--- 数据库.sql
+++ 数据库.sql
@@ -8,16 +8,17 @@ create table 留言 (
   时间 timestamp not null,
   标题 varchar(20) not null,
   内容 text not null,
+  回复 text not null,
   邮箱 varchar(60) not null,
   ip varchar(15) not null
 );
 
 insert into 留言(标题, 内容, 邮箱, ip) values ('测试','测试留言板','i@star2000.work','127.0.0.1');
 
-create table 管理 (
+create table 管理员 (
   编号 int(10) unsigned not null auto_increment primary key,
   名字 varchar(20) not null,
   密码 varchar(40) not null
 );
 
-insert into 管理(名字, 密码) values ('李星',sha1(1753304));
+insert into 管理员(名字, 密码) values ('李星',sha1(1753304));
