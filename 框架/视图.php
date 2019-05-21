a4439ae7241709835545353f092bc16fc937ac6d
星 <i@star2000.work>
2019-05-21 13:47:48 +0800

    添加过滤函数

--- 框架/视图.php
+++ 框架/视图.php
@@ -13,6 +13,11 @@ class 视图
      */
     public function 渲染(array $数据 = [])
     {
+        array_walk_recursive($数据, function (&$值) {
+            if (is_string($值)) {
+                $值 = nl2br(htmlentities($值));
+            }
+        });
         extract($数据, EXTR_OVERWRITE);
         require __DIR__ . '/../应用/' . $GLOBALS['应用'] . '/视图/' . $GLOBALS['行为'] . '.php';
     }
