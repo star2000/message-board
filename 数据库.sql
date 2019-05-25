856b874bf206daaa41c869b3197d1cb91a8a8244
星 <i@star2000.work>
2019-05-25 17:50:41 +0800

    指定字符集为utf8

--- 数据库.sql
+++ 数据库.sql
@@ -1,10 +1,10 @@
 set names utf8;
 drop database if exists msg;
-create database msg;
+create database msg charset utf8;
 use msg;
 
 create table 留言 (
-  编号 int(10) unsigned not null auto_increment primary key,
+  编号 int unsigned auto_increment primary key,
   时间 timestamp not null,
   标题 varchar(20) not null,
   内容 text not null,
@@ -16,7 +16,7 @@ create table 留言 (
 insert into 留言(标题, 内容, 邮箱, ip) values ('测试','测试留言板','i@star2000.work','127.0.0.1');
 
 create table 管理员 (
-  编号 int(10) unsigned not null auto_increment primary key,
+  编号 int unsigned auto_increment primary key,
   名字 varchar(20) not null,
   密码 varchar(40) not null
 );
