2a3651ba7212a0480ceac3fbad957ce507c1de33
星 <i@star2000.work>
2019-05-27 10:40:35 +0800

    转换为小写

--- 应用/公共/视图/列表.php
+++ 应用/公共/视图/列表.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>留言板</title>
     <link rel="shortcut icon" href="favicon.ico" type="image/png">
     <link href="bootstrap.css" rel="stylesheet">
