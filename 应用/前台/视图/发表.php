bd1ba4d41f16bbd5807042dedefc1b4b5acfc0fc
星 <i@star2000.work>
2019-05-27 10:35:04 +0800

    提交改为发表

--- 应用/前台/视图/发表.php
+++ 应用/前台/视图/发表.php
@@ -16,6 +16,6 @@
         <input name="标题" placeholder="标题" class="form-control"><br>
         <textarea name="内容" placeholder="内容" rows="10" class="form-control"></textarea><br>
         <input type="email" name="邮箱" placeholder="邮箱" class="form-control"><br>
-        <button type="submit" class="btn btn-block btn-outline-primary">提交</button>
+        <button type="submit" class="btn btn-block btn-outline-primary">发表</button>
     </form>
 </body>
