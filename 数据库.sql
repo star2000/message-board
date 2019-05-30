a1573401c94e7d0445e54c5af9902f59ac8e1efa
星 <i@star2000.work>
2019-05-30 14:00:34 +0800

    删除'标题'列

--- 数据库.sql
+++ 数据库.sql
@@ -6,14 +6,12 @@ use msg;
 create table 留言 (
   编号 int unsigned auto_increment primary key,
   时间 timestamp not null,
-  标题 varchar(20) not null,
   内容 text not null,
-  回复 text not null,
   邮箱 varchar(60) not null,
   ip varchar(15) not null
 );
 
-insert into 留言(标题, 内容, 邮箱, ip) values ('测试','测试留言板','i@star2000.work','127.0.0.1');
+insert into 留言(内容, 邮箱, ip) values ('测试留言板','i@star2000.work','127.0.0.1');
 
 create table 管理员 (
   编号 int unsigned auto_increment primary key,
